70ebdd86968dd4f963fd2181e59c3aa99b1d6335
Hasan Altintas <66451808+Tolunay-c@users.noreply.github.com>
2024-08-20 18:15:33 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,48 @@
-var swiper = new Swiper(".mySwiper", {
-    loop: true,
+
+
+
+  function incrementValue() {
+    var value = parseInt(document.getElementById('quantity').value, 10);
+    value = isNaN(value) ? 1 : value;
+    value++;
+    document.getElementById('quantity').value = value;
+  }
+
+  function decrementValue() {
+    var value = parseInt(document.getElementById('quantity').value, 10);
+    value = isNaN(value) ? 1 : value;
+    value = value <= 1 ? 1 : value - 1;
+    document.getElementById('quantity').value = value;
+  }
+
+  function validateInput() {
+    var value = parseInt(document.getElementById('quantity').value, 10);
+    if (isNaN(value) || value < 1) {
+      document.getElementById('quantity').value = 1;
+    }
+  }
+
+
+  var swiper = new Swiper(".mySwiper", {
     spaceBetween: 10,
     slidesPerView: 4,
     freeMode: true,
     watchSlidesProgress: true,
+    direction: "vertical", // Thumbnail sliderın dikey olarak hizalanması
   });
+  
   var swiper2 = new Swiper(".mySwiper2", {
-    loop: true,
     spaceBetween: 10,
+    thumbs: {
+      swiper: swiper,
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    thumbs: {
-      swiper: swiper,
-    },
   });
+  
+
+  $(document).ready(function(){
+    $('.card-number').inputmask('9999 9999 9999 9999'); // Kredi kartı formatı
+});
