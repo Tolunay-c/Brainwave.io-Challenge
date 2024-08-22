7498efff4961cdf307fe06aad64573794faa0dfb
Tolunay-c <tolunaycagl@hotmail.com>
2024-08-22 14:10:52 +0300

    upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,64 @@
+/*input functions start*/
 
+function incrementValue() {
+  var value = parseInt(document.getElementById("quantity").value, 10);
+  value = isNaN(value) ? 1 : value;
+  value++;
+  document.getElementById("quantity").value = value;
+}
 
-/*input functions start*/ 
-  function incrementValue() {
-    var value = parseInt(document.getElementById('quantity').value, 10);
-    value = isNaN(value) ? 1 : value;
-    value++;
-    document.getElementById('quantity').value = value;
+function decrementValue() {
+  var value = parseInt(document.getElementById("quantity").value, 10);
+  value = isNaN(value) ? 1 : value;
+  value = value <= 1 ? 1 : value - 1;
+  document.getElementById("quantity").value = value;
+}
+
+function validateInput() {
+  var value = parseInt(document.getElementById("quantity").value, 10);
+  if (isNaN(value) || value < 1) {
+      document.getElementById("quantity").value = 1;
   }
-
-  function decrementValue() {
-    var value = parseInt(document.getElementById('quantity').value, 10);
-    value = isNaN(value) ? 1 : value;
-    value = value <= 1 ? 1 : value - 1;
-    document.getElementById('quantity').value = value;
-  }
-
-  function validateInput() {
-    var value = parseInt(document.getElementById('quantity').value, 10);
-    if (isNaN(value) || value < 1) {
-      document.getElementById('quantity').value = 1;
-    }
-  }
+}
 /*input functions over*/
 
-  var swiper = new Swiper(".productDetailThumbnail", {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-    direction: "vertical", // Thumbnail sliderın dikey olarak hizalanması
-  });
-  
-  var swiper2 = new Swiper(".productDetailSlider", {
-    spaceBetween: 10,
-    thumbs: {
-      swiper: swiper,
-    },
-  });
-  
-  function togglePlan() {
-    const planDurationElements = document.querySelectorAll('.plan-duration');
-    const billedPeriodElements = document.querySelectorAll('.billed-period');
-    
-    planDurationElements.forEach(element => {
-      element.textContent = element.textContent === 'month' ? 'year' : 'month';
-    });
-  
-    billedPeriodElements.forEach(element => {
-      element.textContent = element.textContent === 'billed monthly' ? 'billed yearly' : 'billed monthly';
-    });
-  }
-  
-
-  $(document).ready(function(){
-    $('.card-number').inputmask('9999 9999 9999 9999'); 
+var swiper = new Swiper(".productDetailThumbnail", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+  direction: "vertical", // Thumbnail sliderın dikey olarak hizalanması
 });
-$(document).ready(function(){
-  $('.month-year-input').inputmask('99/99', { 
-      placeholder: 'MM/YY',  
-      alias: 'datetime',
-      inputFormat: 'mm/yy',
-      separator: '/',
-      clearIncomplete: true 
+
+var swiper2 = new Swiper(".productDetailSlider", {
+  spaceBetween: 10,
+  thumbs: {
+      swiper: swiper,
+  },
+});
+
+function togglePlan() {
+  const planDurationElements = document.querySelectorAll(".plan-duration");
+  const billedPeriodElements = document.querySelectorAll(".billed-period");
+
+  planDurationElements.forEach((element) => {
+      element.textContent = element.textContent === "month" ? "year" : "month";
+  });
+
+  billedPeriodElements.forEach((element) => {
+      element.textContent = element.textContent === "billed monthly" ? "billed yearly" : "billed monthly";
+  });
+}
+
+$(document).ready(function () {
+  $(".card-number").inputmask("9999 9999 9999 9999");
+});
+$(document).ready(function () {
+  $(".month-year-input").inputmask("99/99", {
+      placeholder: "MM/YY",
+      alias: "datetime",
+      inputFormat: "mm/yy",
+      separator: "/",
+      clearIncomplete: true,
   });
 });
